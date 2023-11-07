e465a9a0b8c9007755f3e5d8f7e1cf7180ab1167
fejesmartin <fejes.martin00@gmail.com>
2023-11-07 16:31:17 +0100

    feature: adds AuthenticationService

---
+++ Codecool.BruteForce/Authentication/AuthenticationService.cs
@@ -0,0 +1,26 @@
+﻿using Codecool.BruteForce.Users.Repository;
+
+namespace Codecool.BruteForce.Authentication;
+
+public class AuthenticationService: IAuthenticationService
+{
+    private readonly IUserRepository _userRepository;
+    
+    public AuthenticationService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    public bool Authenticate(string userName, string password)
+    {
+        int userId;
+        if (int.TryParse(userName[^1].ToString(), out userId))
+        {
+            var user = _userRepository.Get(userId);
+            if (user != null && user.UserName == userName && user.Password == password)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
