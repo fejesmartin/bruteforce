35d78aced68862ff96ead3a294fc5d1767b2e7c3
fejesmartin <fejes.martin00@gmail.com>
2023-11-19 14:48:28 +0100

    refactor: PasswordBreaker

--- Codecool.BruteForce/Passwords/Breaker/PasswordBreaker.cs
+++ Codecool.BruteForce/Passwords/Breaker/PasswordBreaker.cs
@@ -8,49 +8,55 @@ public class PasswordBreaker : IPasswordBreaker
     private  readonly AsciiTableRange UppercaseChars = new(65, 90);
     private  readonly AsciiTableRange Numbers = new(48, 57);
   
-    public IEnumerable<string> GetCombinations(int passwordLength)
-    {
-
-        List<List<string>> allChars = new();
-         
-        if (passwordLength <= 0)
-        {
-            throw new ArgumentException("Password length must be greater than 0.");
-        }
-
-        List<string> numberList = new();
-        for (int i = Numbers.Start; i <= Numbers.End; i++)
-        {
-            numberList.Add(((char)i).ToString());
-        }
-        List<string> lowerCaseList = new();
-        for (int i = LowercaseChars.Start; i <= LowercaseChars.End; i++)
-        {
-            lowerCaseList.Add(((char)i).ToString());
-        }
-        List<string> upperCaseList = new();
-        for (int i = UppercaseChars.Start; i <= UppercaseChars.End; i++)
-        {
-            upperCaseList.Add(((char)i).ToString());
-        }
-
-        allChars.Add(numberList);
-        allChars.Add(lowerCaseList);
-        allChars.Add(upperCaseList);
-       
-
-        return GetAllPossibleCombos(allChars);
-
-    }
-
     private static IEnumerable<string> GetAllPossibleCombos(IEnumerable<IEnumerable<string>> strings)
     {
         IEnumerable<string> combos = new[] { "" };
 
         combos = strings
-            .Aggregate(combos, (current, inner) => 
-                current.SelectMany(c => inner, (c, i) => c + i));
+            .Aggregate(combos, (current, inner) => current.SelectMany(c => inner, (c, i) => c + i));
 
         return combos;
-    }  
+    }
+
+    public IEnumerable<string> GetCombinations(int passwordLength)
+    {
+        if (passwordLength <= 0)
+        {
+            throw new ArgumentException("Password length must be greater than 0.");
+        }
+
+        Console.WriteLine($"Generating combinations for password length: {passwordLength}");
+
+        List<string> allChars = new List<string>();
+
+        for (int i = Numbers.Start; i <= Numbers.End; i++)
+        {
+            allChars.Add(((char)i).ToString());
+        }
+
+        for (int i = LowercaseChars.Start; i <= LowercaseChars.End; i++)
+        {
+            allChars.Add(((char)i).ToString());
+        }
+
+        for (int i = UppercaseChars.Start; i <= UppercaseChars.End; i++)
+        {
+            allChars.Add(((char)i).ToString());
+        }
+
+        Console.WriteLine($"Total characters: {allChars.Count}");
+
+        IEnumerable<string> combos = new[] { "" };
+
+        for (int i = 0; i < passwordLength; i++)
+        {
+            combos = combos.SelectMany(c => allChars, (c, j) => c + j);
+        }
+
+        Console.WriteLine($"Total combinations: {combos.Count()}");
+
+        return combos;
+    }
+
+
 }
